f0295354806c07659e55b39f251da6c1ba47a932
Christian Bernhard <chris@dreamride.de>
2012-11-07 11:13:46 +0100

    uploaded all necessary module files

---
+++ trustedshopsrich/metadata.php
@@ -0,0 +1,13 @@
+ <?php
+$aModule = array(
+    'id'              => 'trustedshopsrich',
+    'title'           => 'Trustes Shops Rich Snippet',
+    'description'     => 'Gibt die Bewertungen in Textform als Rich Snippe aus.',
+    'version'         => '1.0.2',
+    'author'          => 'Christian Bernhard',
+    'url'             => 'http://www.meinestruempfe.de',
+    'email'           => 'chris@dreamride.de',
+    'extend'           => array(
+           'oxviewconfig' => 'trustedshopsrich/trustedshopsrich'
+           )
+);
