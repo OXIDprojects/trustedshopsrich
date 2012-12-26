146475868ddb6362627e4e22d775a971a790fcd8
coarsy <chris@dreamride.de>
2012-12-26 17:40:14 +0100

    added new divs for better html output structuring

--- copy_this/modules/trustedshopsrich/trustedshopsrich.php
+++ copy_this/modules/trustedshopsrich/trustedshopsrich.php
@@ -1,38 +1,49 @@
 <?php
+
 class trustedshopsrich extends trustedshopsrich_parent
 {
 	public function getTrustedShopsRich($tsId, $type = 1)
 	{
-		$oShop = $this->getConfig()->getActiveShop();
-		$sShopTitle = $oShop->oxshops__oxname->getRawValue();
-
-		$cacheFile = getShopBasePath().'tmp/trustedshops.cache';
-		if (file_exists($cacheFile)) {
-			$now = time();
-			$then = filemtime($cacheFile);
-			if ($now-$then > 60*60) {
-				unlink($cacheFile);
-			} else {
-				$xml = simplexml_load_string(file_get_contents($cacheFile));
-			} 
-		}
-		if (!is_object($xml) && $xml = simplexml_load_file("https://www.trustedshops.com/bewertung/show_xml.php?tsid=".$tsId)) {
-			file_put_contents($cacheFile, $xml->asXML());
-		}
 		
-		if (is_object($xml)) {
-			$max = $xml->ratings->result[0];
-			$average = $xml->ratings->result[1];
-			$count = $xml->ratings["amount"];
-			
-			$htmlOutput = '<div class="hreview-aggregate"><div class="item"><span class="fn">' . $sShopTitle . '</span></div><span class="rating">Durchschnittliche Bewertung: <span class="average">' . $average . '</span> (von 5). Ermittelt aus  <span class="votes">' . $count . '</span> <a class="links_link" href="https://www.trustedshops.de/bewertung/info_' . $tsId . '.html">Bewertungen</a>. </span></div>';
-			
-			$htmlOutput2 = '<a href="http://www.trustedshops.de/shop-info/trusted-shops-kundenbewertungen/">Kundenbewertungen von Trusted Shops</a>:
-			<span xmlns:v="http://rdf.data-vocabulary.org/#" typeof="v:Review-aggregate">
-			    <span rel="v:rating"><span property="v:value">' . $average . '</span>
-			    </span> / <span property="v:best">' . $max . '
-			    </span> bei <span property="v:count">' . $count . '</span> <a href="https://www.trustedshops.de/bewertung/info_' . $tsId . '.html">Bewertungen</a>
-			</span>';
+	$oShop = $this->getConfig()->getActiveShop();
+  $sShopTitle = $oShop->oxshops__oxname->getRawValue();
+  
+  $cacheFile = getShopBasePath().'tmp/trustedshops.cache';
+  
+  if (file_exists($cacheFile)) {
+    $now = time();
+    $then = filemtime($cacheFile);
+    if ($now-$then > 60*60) {
+      unlink($cacheFile);
+    } else {
+      $xml = simplexml_load_string(file_get_contents($cacheFile));
+    }
+  }
+  
+  if (!is_object($xml) && $xml = simplexml_load_file("https://www.trustedshops.com/bewertung/show_xml.php?tsid=".$tsId)) {
+    file_put_contents($cacheFile, $xml->asXML());
+  }
+		
+	if (is_object($xml)) {
+	
+		$max = $xml->ratings->result[0];
+		$average = $xml->ratings->result[1];
+		$count = $xml->ratings["amount"];
+		
+		$htmlOutput = '
+    <div class="trustedshops">
+      <div class="hreview-aggregate"><div class="item"><span class="fn">' . $sShopTitle . '</span></div><span class="rating">Durchschnittliche Bewertung: <span class="average">' . $average . '</span> (von 5). Ermittelt aus  <span class="votes">' . $count . '</span> Bewertungen. </span></div>
+    </div>';
+		
+		$htmlOutput2 = '
+    <div class="trustedshops">Kundenbewertungen von ' . $sShopTitle . '<br>
+  		<span xmlns:v="http://rdf.data-vocabulary.org/#" typeof="v:Review-aggregate">
+  		    Durchschnittliche Bewertung: <span rel="v:rating"><span property="v:value">' . $average . '</span>
+  		    </span> / <span property="v:best">' . $max . '
+  		    </span> Ermittelt aus <span property="v:count">' . $count . '</span> <a href="https://www.trustedshops.de/bewertung/info_' . $tsId . '.html" rel="nofollow">Bewertungen</a>.
+  		</span>
+    </div>';			
+		
 		} else {
 			$htmlOutput = "No connection to Trusted Shops.";
 		}
@@ -50,6 +61,7 @@ class trustedshopsrich extends trustedshopsrich_parent
 			default:
 				$htmlOutput = $htmlOutput;
 		}
-		return $htmlOutput;
+			
+	return $htmlOutput;
 	}
 }
